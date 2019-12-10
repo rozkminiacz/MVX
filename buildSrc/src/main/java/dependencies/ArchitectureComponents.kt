6511997aa84795fe8ab6fe391a0326861f12e8df
jaroslawmichalik <michalik@protonmail.ch>
2019-12-10 22:26:55 +0100

    Update androidx versions

--- buildSrc/src/main/java/dependencies/ArchitectureComponents.kt
+++ buildSrc/src/main/java/dependencies/ArchitectureComponents.kt
@@ -5,26 +5,21 @@ package dependencies
  */
 object ArchitectureComponents : DependencyContainer {
 
-  val browserVersion = "1.0.0-beta01"
 
-  val workManager = "android.arch.work:work-runtime:1.0.0-beta01"
-  val design = "com.google.android.material:material:1.0.0"
-  val recyclerView = "androidx.recyclerview:recyclerview:1.0.0"
-  val appCompat = "androidx.appcompat:appcompat:1.0.0"
-  val support = "androidx.legacy:legacy-support-v4:1.0.0"
-  val constraint = "androidx.constraintlayout:constraintlayout:1.1.2"
-  val browser = "androidx.browser:browser:$browserVersion"
+    val design = "com.google.android.material:material:1.0.0"
+    val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
+    val appCompat = "androidx.appcompat:appcompat:1.1.0"
+    val constraint = "androidx.constraintlayout:constraintlayout:1.1.3"
+    val browser = "androidx.browser:browser:1.0.0"
 
-  override val all: List<String> = listOf(
-      workManager,
-      design,
-      recyclerView,
-      appCompat,
-      support,
-      constraint,
-      browser
-  )
+    override val all: List<String> = listOf(
+        design,
+        recyclerView,
+        appCompat,
+        constraint,
+        browser
+    )
 
-  override val kapt: String = "com.android.databinding:compiler:3.2.1"
+    override val kapt: String = "com.android.databinding:compiler:3.5.0"
 
 }
